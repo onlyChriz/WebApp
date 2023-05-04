7188a430a21455426b34127f1f51e8eda433434f
Jiaojiao Song <ssssjulia525@gmail.com>
2023-05-04 17:26:05 +0200

    index page

--- WebApp/WebApp/Pages/Index.cshtml.cs
+++ WebApp/WebApp/Pages/Index.cshtml.cs
@@ -1,20 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Data;
 
 namespace WebApp.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        ApplicationDbContext applicationDbContext { get; set; }
+        public List<BlogPosts> BlogPosts { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ApplicationDbContext applicationDbContext)
         {
-            _logger = logger;
+            this.applicationDbContext = applicationDbContext;
         }
 
         public void OnGet()
         {
-
+            BlogPosts = applicationDbContext.BlogPosts.ToList();
         }
     }
 }
--- WebApp/WebApp/Pages/Models/Domain/Posts.cs
+++ WebApp/WebApp/Pages/Models/Domain/Posts.cs
@@ -6,6 +6,6 @@
         public string Title { get; set; }
         public string Content { get; set; }
         public DateTime PostDate { get; set; }
-
+        public string Author { get; set; }
     }
 }
